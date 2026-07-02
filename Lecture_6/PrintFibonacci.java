aa103c261b6ccb36c2afb743380c1f46a3350c3d
suvamgurung <gurungsuvam265@gmail.com>
2026-07-02 18:54:11 +0545

    Add: Fibonacci sequence

--- Lecture_6/PrintFibonacci.java
+++ Lecture_6/PrintFibonacci.java
@@ -1,4 +1,20 @@
 package Lecture_6;
-
+/**1 1 2 3 5 8 13*/
 public class PrintFibonacci {
+    public static void fibonacci(int num) {
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < num; i++) {
+            System.out.print(b + " ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+
+
+        }
+    }
+    public static void main(String[] args) {
+        fibonacci(7);
+    }
 }
