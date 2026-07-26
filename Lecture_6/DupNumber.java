a5f8dac0c04fc591599446664b15032b988bebab
suvamgurung <gurungsuvam265@gmail.com>
2026-07-26 23:57:28 +0545

    Update: Try with sorting

--- Lecture_6/DupNumber.java
+++ Lecture_6/DupNumber.java
@@ -1,23 +1,25 @@
 package Lecture_6;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class DupNumber {
     public boolean hasDuplicate(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
+        Arrays.sort(nums);
 
-        for (int num: nums) {
-            if (!seen.add(num)) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
         }
         return false;
     }
 
+
     public static void main(String[] args) {
         DupNumber dupNumber = new DupNumber();
-        int[] nums = {1,2,3,3,4};
+        int[] nums = {1, 2, 3, 3, 4};
         System.out.println(dupNumber.hasDuplicate(nums));
     }
 }
